2855f6829b110d8aacf90d2204de8f8262306b45
manivel <gsmanivel@gmail.com>
2020-06-11 23:19:01 +0530

    Added a comment

--- src/main/java/com/manman/Rest2020/Rest2020Application.java
+++ src/main/java/com/manman/Rest2020/Rest2020Application.java
@@ -3,6 +3,7 @@ package com.manman.Rest2020;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//THis is spring boot class in gzerobase
 @SpringBootApplication
 public class Rest2020Application {
 
