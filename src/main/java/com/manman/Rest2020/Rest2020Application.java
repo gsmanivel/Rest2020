386fbbaaa785c11ae074fe9eefbe09928605b944
manivel <gsmanivel@gmail.com>
2020-06-11 22:43:27 +0530

    initial Commit

---
+++ src/main/java/com/manman/Rest2020/Rest2020Application.java
@@ -0,0 +1,13 @@
+package com.manman.Rest2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rest2020Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Rest2020Application.class, args);
+  }
+
+}
---
+++ src/test/java/com/manman/Rest2020/Rest2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manman.Rest2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rest2020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
